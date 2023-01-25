7a158e7e415e9cf4b266f1b84b8193032407435b
Fennec92 <nazar.hamidulla@posteo.de>
2023-01-25 23:30:39 +0100

    final structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import { Home, Create } from "./pages/exportPages";
 import { Header, Main } from "./components/exportComponents";
 
 const App = () => {
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Home, Create } from "../pages/exportPages";
+
 const Main = () => {
-    return <main className="w-full bg-[#f9fafe] px-4 py-8 sm:p-8"></main>;
+    return (
+        <main className="min-h-[calc(100vh-74px)] w-full bg-[#f9fafe] px-4 py-8 sm:p-8">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/create" element={<Create />} />
+            </Routes>
+        </main>
+    );
 };
 export default Main;
