bee93725b232a27aa39df6448a56cc743905cc6e
Fennec92 <nazar.hamidulla@posteo.de>
2023-01-26 01:24:37 +0100

    structure for showing images and render function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,19 @@ import { useState, useEffect } from "react";
 
 import { Card, Loader, FormField } from "../components/exportComponents";
 
+const renderCards = ({ data, title }) => {
+    if (data?.length > 0) {
+        const cardsToRender = data.map((item) => (
+            <Card key={item._id} {...item} />
+        ));
+        return cardsToRender;
+    }
+};
+
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
+    const [searchText, setSearchText] = useState("");
 
     return (
         <section className="mx-auto max-w-7xl">
@@ -21,6 +31,26 @@ const Home = () => {
 
             <div className="mt-16">
                 <FormField />
+            </div>
+
+            <div className="mt-10">
+                {loading ? (
+                    <div className="flex items-center justify-center">
+                        <Loader />
+                    </div>
+                ) : (
+                    <>
+                        {searchText && (
+                            <h2 className="mb-3 text-xl font-medium text-[#666e75]">
+                                Generierte Ergebnisse für{" "}
+                                <span className="text-[#222328]">
+                                    {searchText}
+                                </span>
+                            </h2>
+                        )}
+                        <div className="grid grid-cols-1 gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4"></div>
+                    </>
+                )}
             </div>
         </section>
     );
