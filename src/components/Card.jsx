a9db3725249f6cd3a8247c89898be942009e0667
Fennec92 <nazar.hamidulla@posteo.de>
2023-01-31 01:08:44 +0100

    finished card layout and download functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,11 +10,26 @@ const Card = ({ _id, name, description, image }) => {
                 className="h-auto w-full rounded-xl object-cover"
             />
             <div className="absolute bottom-0 left-0 right-0 m-2 max-h-[50%] flex-col rounded-md bg-[#10131f] p-4 group-hover:flex">
-                <p className="description overflow-y-auto text-sm text-white">
+                <p className="description text-md overflow-y-auto text-white">
                     {description}
                 </p>
-                <div className="flex items-center gap-2">
-                    <div className="h-7 w-7 bg-green-700">{name[0]}</div>
+                <div className="mt-5 flex items-center justify-between gap-2">
+                    <div className="flex items-center gap-2">
+                        <div className="flex h-7 w-7 items-center justify-center rounded-full bg-green-700 text-xs font-bold text-white">
+                            {name[0].toUpperCase()}
+                        </div>
+                        <p className="text-sm text-white">{name}</p>
+                    </div>
+                    <button
+                        type="button"
+                        onClick={() => downloadImage(_id, image)}
+                    >
+                        <img
+                            className="h-6 w-6 invert"
+                            src={download}
+                            alt="download"
+                        />
+                    </button>
                 </div>
             </div>
         </div>
